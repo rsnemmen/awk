ad5a96fbaa46b6dc0cfa6adea02077b81d6d3707
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-04-12 06:06:21 -0300

    colors\!

--- findFunctions.sh
+++ findFunctions.sh
@@ -2,13 +2,18 @@
 #
 # Lists all C source files and the functions defined inside them.
 #
+# ./findFunctions.sh 
+
+# colors and boldface, cf. https://stackoverflow.com/a/20983251/793218
+fmt=`tput bold`
+reset=`tput sgr0`
 
 # finds dir where this script is located
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for X in `ls *.c`
     do
-    	echo $X
+    	echo "${fmt}$X ${reset}"
     	awk -f $DIR/findFunctions.awk $X
     	echo
     done
